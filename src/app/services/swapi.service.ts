9d8d6d134369df1aba1741070b24d0230c879b1e
Anna Melnyk <blairw73@gmail.com>
2024-04-22 22:34:08 +1000

    Add favicon

--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -1,24 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
-import { IBaseUrl, SwapiResource } from '../data/models/base-url';
+import { IBaseUrl, SwapiResource } from '../data/models/base-url'
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwapiService {
   private apiUrl: string = 'https://swapi.dev/api';
+  favourites: any[] = []
 
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient) { }
 
 
   getData(resource: string, page: number = 1): Observable<IBaseUrl> {
-    return this._http.get<IBaseUrl>(`${this.apiUrl}/${resource}?page=${page}`);
+    return this._http.get<IBaseUrl>(`${this.apiUrl}/${resource}?page=${page}`)
   }
 
   getDataItem(resource: SwapiResource, index: number) {
-  return this._http.get(`${this.apiUrl}/${resource}/${index}`)
+    return this._http.get(`${this.apiUrl}/${resource}/${index}`)
   }
+
+
 }
