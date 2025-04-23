4d391cd95ba2600e8708cf733f6805456737ad10
Anna Melnyk <blairw73@gmail.com>
2025-04-23 22:48:03 +1000

    Auto-generated commit

--- src/app/pages/films/films.routes.ts
+++
@@ -1,5 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { FilmsComponent } from './films.component';
-
-export const FILMS_ROUTES: Routes = [{ path: '', component: FilmsComponent }];
--- src/app/pages/spaceships/spaceships.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-spaceships',
-  template: `<app-main-items-list-block collectionName='starships'></app-main-items-list-block>`,
-})
-export class SpaceshipsComponent {}
